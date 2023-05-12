fc8c634253d14eda6031ae12add9a0dfca9f91c2
Mannan Shaikh <arsalaanshaikh8@gmail.com>
2023-05-13 01:15:14 +0530

    created DB instance factory

--- Mobile Store/Models/User.cs
+++ Mobile Store/Models/User.cs
@@ -1,6 +1,10 @@
-﻿using Mobile_Store.Interfaces;
+﻿using Mobile_Store.DBFactory;
+using Mobile_Store.Interfaces;
 using Mobile_Store.Structures;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Mobile_Store.Models
 {
@@ -17,17 +21,40 @@ namespace Mobile_Store.Models
         }
         #endregion
 
+        #region Private Variables
+        /// <summary>
+        /// Private variables to initialize in constructor
+        /// </summary>
+        private readonly SingletonDBFactory _singletonDBFactory;
+        private readonly string ConnectionString;
+        private readonly SqlConnection sqlConnection;
+        private SqlCommand sqlCommand;
+        #endregion
+
+        #region Class Instance constructor
+        /// <summary>
+        /// Class Instance constructor
+        /// </summary>
+        /// <param name="singletonDBFactory"></param>
+        User(SingletonDBFactory singletonDBFactory)
+        {
+            _singletonDBFactory = singletonDBFactory;
+            ConnectionString = _singletonDBFactory.DbInstance();
+            sqlConnection = new SqlConnection(ConnectionString);
+        }
+        #endregion
+
         #region Public Properties
         /// <summary>
         /// Public Properties
         /// </summary>
-        public Guid ID { get; set; }
+        public Guid? ID { get; set; }
         public Name Name { get; set; }
-        public Address? Address { get; set; }
+        public Address Address { get; set; }
         public string? Email { get; set; }
-        public Role RoleId { get; set; }
+        public Role? RoleId { get; set; }
         public DateOnly? DateOfBirth { get; set; }
-        public bool Permission { get; set; }
+        public bool? Permission { get; set; }
         public string? UserName { get; set; }
         public string? Password { get; set; }
         #endregion
@@ -40,6 +67,15 @@ namespace Mobile_Store.Models
         /// <returns> Boolean Value based on task completion </returns>
         public bool AddUser(User user)
         {
+            using (sqlCommand = new SqlCommand("PROC NAME", sqlConnection))
+            {
+                sqlConnection.Open();
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("", SqlDbType.NVarChar).Value = user.Name.FirstName;
+                sqlCommand.Parameters.AddWithValue("", SqlDbType.NVarChar).Value = user.Address.City;
+                sqlCommand.ExecuteNonQuery();
+                sqlConnection.Close();
+            }
             return true;
         }
 
@@ -50,6 +86,15 @@ namespace Mobile_Store.Models
         /// <returns> Boolean Value based on task completion </returns>
         public bool EditUser(User user)
         {
+            using (sqlCommand = new SqlCommand("PROC NAME", sqlConnection))
+            {
+                sqlConnection.Open();
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("", SqlDbType.NVarChar).Value = user.Name.FirstName;
+                sqlCommand.Parameters.AddWithValue("", SqlDbType.NVarChar).Value = user.Address.City;
+                sqlCommand.ExecuteNonQuery();
+                sqlConnection.Close();
+            }
             return true;
         }
 
@@ -60,6 +105,15 @@ namespace Mobile_Store.Models
         /// <returns> Boolean Value based on task completion </returns>
         public bool DeleteUser(User user)
         {
+            using (sqlCommand = new SqlCommand("PROC NAME", sqlConnection))
+            {
+                sqlConnection.Open();
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("", SqlDbType.NVarChar).Value = user.Name.FirstName;
+                sqlCommand.Parameters.AddWithValue("", SqlDbType.NVarChar).Value = user.Address.City;
+                sqlCommand.ExecuteNonQuery();
+                sqlConnection.Close();
+            }
             return true;
         }
 
@@ -70,6 +124,15 @@ namespace Mobile_Store.Models
         /// <returns> User type object, with the fetched details </returns>
         public User SearchUser(User user)
         {
+            using (sqlCommand = new SqlCommand("PROC NAME", sqlConnection))
+            {
+                sqlConnection.Open();
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("", SqlDbType.NVarChar).Value = user.Name.FirstName;
+                sqlCommand.Parameters.AddWithValue("", SqlDbType.NVarChar).Value = user.Address.City;
+                sqlCommand.ExecuteNonQuery();
+                sqlConnection.Close();
+            }
             return new User() { ID = Guid.NewGuid(), Name = new Name() { FirstName = "", MiddleName = null, LastName = ""}, Address = new Address() { State = "", City = "", Country = "", ZIP_Code = 0 }, Email = "" };
         }
         #endregion
